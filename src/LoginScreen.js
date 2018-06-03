0a4bfa9d71c9a307e6cc802adb35029116197039
saketkumar95 <saket.singh1410@gmail.com>
2018-06-03 13:51:19 +0530

    Fix errors

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -64,7 +64,7 @@ export default class LoginScreen extends Component {
             }}>
                 <TouchableOpacity
                     onPress= {() => this.decreaseHeight()} >
-                        {/* <Icon name="md-arrow-back" style={{color: 'black'}} /> */}
+                        <Icon name="md-arrow-back" style={{color: 'black'}} />
                 </TouchableOpacity>
             </Animated.View>
                 <ImageBackground
